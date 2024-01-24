f2defd4aa31f42da7d672ae98f474769dc6bba49
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2024-01-24 13:56:51 -0300

    .

--- consilta.sql
+++ consilta.sql
@@ -1,10 +1,10 @@
-select T.Origem, R.Nome as 'Sup', Es.Nivel_03 as 'CR ', TerminoReal as 'Data de Finalização' 
-from Tarefa T with(nolock)
-inner join Recurso R with(nolock) on R.CodigoHash = T.FinalizadoPorHash
-inner join DW_Vista.dbo.DM_Estrutura Es with(nolock) on Es.Id_Estrutura = T.EstruturaId
-inner join DW_Vista.dbo.DM_CR cr with(nolock) on cr.Id_CR = Es.ID_Cr
-where Cr.Gerente = 'DENISE DOS SANTOS DIAS SILVA'
-and DAY(T.TerminoReal) = 18 
+select R.Nome as 'Sup', Es.Nivel_03 as 'CR ', TerminoReal as 'Data de Finalização' 
+from Tarefa T
+inner join Recurso R on R.CodigoHash = T.FinalizadoPorHash
+inner join DW_Vista.dbo.DM_Estrutura Es on Es.Id_Estrutura = T.EstruturaId
+inner join DW_Vista.dbo.DM_CR cr on cr.Id_CR = Es.ID_Cr
+where T.Nome LIKE '%Visita%'
+and Cr.Gerente = 'DENISE DOS SANTOS DIAS SILVA'
+and DAY(T.TerminoReal) = 22
 and MONTH(T.TerminoReal) = 01
 and YEAR(T.TerminoReal) = 2024
-order by TerminoReal DESC
