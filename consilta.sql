7e8f66d5092c0bc49f925af90a92e6c80d3992be
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2024-01-31 09:41:18 -0300

    .

--- consilta.sql
+++ consilta.sql
@@ -16,21 +16,42 @@
 
 -- 30R,32F,48C
 
-select 
-T.Nome, R.Nome as 'Colaborador',
-(CASE WHEN T.Status = 10 THEN 'ABERTA' ELSE
-(CASE WHEN T.Status = 85 THEN 'FINALIZADA' ELSE
-(CASE WHEN T.Status = 25 THEN 'INICIADA' END) END) END) as 'status',
-(CASE WHEN T.Origem = 30 THEN 'ROTINA' ELSE
-(CASE WHEN T.Origem = 32 THEN 'FLUXO' ELSE
-(CASE WHEN T.Origem = 48 THEN 'CHAMADO' END) END) END) as 'Origem', Es.Descricao
+-- select 
+-- T.Nome, R.Nome as 'Colaborador',
+-- (CASE WHEN T.Status = 10 THEN 'ABERTA' ELSE
+-- (CASE WHEN T.Status = 85 THEN 'FINALIZADA' ELSE
+-- (CASE WHEN T.Status = 25 THEN 'INICIADA' END) END) END) as 'status',
+-- (CASE WHEN T.Origem = 30 THEN 'ROTINA' ELSE
+-- (CASE WHEN T.Origem = 32 THEN 'FLUXO' ELSE
+-- (CASE WHEN T.Origem = 48 THEN 'CHAMADO' END) END) END) as 'Origem', Es.Descricao
+-- from Tarefa T with(nolock)
+-- inner join Recurso R on R.CodigoHash = T.FinalizadoPorHash
+-- inner join dw_vista.dbo.DM_ESTRUTURA Es with(nolock) on es.Id_Estrutura = T.EstruturaId
+-- inner join dw_vista.dbo.DM_CR cr with(nolock) on cr.Id_CR = Es.Id_CR
+-- where cr.Gerente = 'denise dos santos dias silva'
+-- and es.PECNo = 098629 
+-- and T.Nome = 'Visita Oper. Liderança'
+-- and MONTH(Disponibilizacao) = 01
+-- and YEAR(Disponibilizacao) = 2024
+-- ORDER BY [Status]
+
+-- select T.Nome, T.Descricao, R.Nome as 'Vigilante', COUNT(R.Nome) as Total
+-- from Tarefa T with(nolock)
+-- inner join Recurso R with(nolock) on R.CodigoHash = T.FinalizadoPorHash
+-- inner join dw_vista.dbo.DM_Estrutura Es with(nolock) on T.EstruturaId = Es.Id_Estrutura
+-- where Es.CRNo = 35900
+-- and DAY(TerminoReal) = 30
+-- and MONTH(TerminoReal) = 01
+-- and YEAR(TerminoReal) = 2024
+-- and R.Nome <> 'Sistema'
+-- GROUP BY T.Nome, T.Descricao, R.Nome
+-- ORDER BY [Total] DESC
+
+set nocount on
+select cr.Gerente, Es.Nivel_03 as 'CR', count(cr.Gerente) as 'Escalonadas'
 from Tarefa T with(nolock)
-inner join Recurso R on R.CodigoHash = T.FinalizadoPorHash
-inner join dw_vista.dbo.DM_ESTRUTURA Es with(nolock) on es.Id_Estrutura = T.EstruturaId
-inner join dw_vista.dbo.DM_CR cr with(nolock) on cr.Id_CR = Es.Id_CR
-where cr.Gerente = 'denise dos santos dias silva'
-and es.PECNo = 098629 
-and T.Nome = 'Visita Oper. Liderança'
-and MONTH(Disponibilizacao) = 01
-and YEAR(Disponibilizacao) = 2024
-ORDER BY [Status]
+inner join dw_vista.dbo.DM_ESTRUTURA Es with(nolock) on Es.Id_estrutura = T.EstruturaId
+inner join dw_vista.dbo.DM_CR cr with(nolock) on cr.Id_cr = es.Id_cr
+where cr.GerenteRegional = 'denise dos santos dias silva'
+and T.Escalonado > 0
+and T.Status <> 85
