580d91d317182a7a53ece1f0192b1ade7d574338
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2024-02-02 17:20:25 -0300

    .

--- parcial.py
+++ parcial.py
@@ -6,7 +6,7 @@ from dataframe_image import export
 from os import system, mkdir
 from sqlalchemy import create_engine
 
-engine = create_engine('mssql://guilherme.breve:84584608Guilherme@10.56.6.56/DW_Vista?driver=SQL Server')
+engine = create_engine('mssql://guilherme.breve:84584608Guilherme@10.56.6.56/Vista_Replication_PRD?driver=SQL Server')
 
 class Parcial:
     def __init__(self):
