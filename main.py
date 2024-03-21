93e1c29f12a01e213dcba6b104e6858775f3a1ab
Guilherme Breve <foxtec198@gmail.com>
2024-03-21 14:42:33 -0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from authWA import Parcial, conectar_email
 
-p = Parcial('guilherme.breve', '84584608@Gui','34.138.244.89', 8, 23)
+# p = Parcial('guilherme.breve', '84584608@Gui','34.138.244.89', 8, 23)
+p = Parcial('guilherme.breve', '84584608@Gui','10.56.6.56', 8, 23)
 conectar_email('foxtec198@gmail.com','vewmksduxchpjirg')
 
 # Dias de Semana
